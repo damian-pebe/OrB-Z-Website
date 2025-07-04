95d7cb6e19daa8a8c12ae9ea7827427e41064b34
damian-pebe <damian.pebe@gmail.com>
2025-07-04 11:52:54 -0600

    add(intl-i18n): integrate next-intl to the app

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world! From OrBz Website With Next.js and Tailwind CSS
-    </h1>
-  )
-}
